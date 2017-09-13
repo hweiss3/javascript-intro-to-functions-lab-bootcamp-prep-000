9cd90fc2857db7c931c0a156e5ef6c8c9ad2db21
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-13 00:54:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string){
-  console.log(string.toUpperCase)
+  console.log(spy.toUpperCase)
 
 }
 var uppercase = "HELLO!"
